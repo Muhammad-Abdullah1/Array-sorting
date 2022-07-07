b62be1c61e84a1203729c8d5aba8fa4beccbe975
Muhammad-Abdullah1 <108523849+Muhammad-Abdullah1@users.noreply.github.com>
2022-07-07 12:35:03 +0500

    Dynamic Table using react and array

---
+++ tableComplete.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react'
+
+const TableHeader = () =>{
+  return(
+  <thead>
+        <tr>
+            <th>ID</th>
+            <th> First Name</th>
+            <th>LastName</th>
+            <th>City</th>
+          </tr>
+        </thead>
+  )
+}
+
+const TableBody = (props) => {
+    const tRows=props.tableData.map((data,index)=>{
+        return(
+            <tr key={index}>
+                <td>{data.ID}</td>
+                <td>{data.FirstName}</td>
+                <td>{data.LastName}</td>
+                <td>{data.City}</td>
+                
+
+            </tr>
+
+        )
+    })
+    return(
+        <tbody>
+      {tRows}
+    </tbody>
+    )}
+
+class TableComplete extends Component {
+  render() {
+    const {tableData} = this.props;
+    return (
+      <table>
+        <TableHeader />
+        <TableBody tableData = {tableData}/>
+      </table>
+    )
+  }
+}
+
+export default TableComplete
